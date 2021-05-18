7564f2c4cb2e5a67e73bb04069e19c33f09c26fb
Klaum <6532053@qq.com>
2021-05-18 21:25:38 +0800

    [ADD]增加数据库连接

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSettings>
 #include "DBLogin.h"
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +27,13 @@ private slots:
 private:
     Ui::MainWindow *ui;
     DBLogin *dbLogin;
+
+    static QSqlDatabase *database;//数据库指针
+    static QSqlQuery *query;//数据库执行指针
+
+    QString configFileName;//配置文件名称
+    //查询当前数据中的表
+    void selectTable();
 
 };
 #endif // MAINWINDOW_H
