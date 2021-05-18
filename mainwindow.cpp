d929c83fc354afcb558d6ed94bb6a8906d0030ab
Klaum <6532053@qq.com>
2021-05-18 22:15:28 +0800

    [ADD]增加查询数据库

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     dbLogin=new DBLogin(this);
+
+    configFileName="DataBases.ini";
+    dataBasesName="";
+
+    sqlQuery=NULL;
+    db = QSqlDatabase::addDatabase("QPSQL");
 }
 
 MainWindow::~MainWindow()
@@ -18,4 +26,59 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     dbLogin->show();
+}
+
+void MainWindow::readINIFile()
+{
+    QString fileName= QCoreApplication::applicationDirPath();
+    fileName+="/"+configFileName;
+    //读取配置文件
+    dbSettings=new QSettings(fileName,QSettings::IniFormat);
+    db.setHostName(dbSettings->value("DATA_BASE/host_name").toString());
+    db.setPort(dbSettings->value("DATA_BASE/port").toString().toUInt());
+    db.setUserName(dbSettings->value("DATA_BASE/user_name").toString());
+    db.setPassword(dbSettings->value("DATA_BASE/password").toString());
+    db.setDatabaseName(dbSettings->value("DATA_BASE/database_name").toString());
+}
+
+void MainWindow::selectTable(){
+    connected = db.open();
+    if(!connected)
+    {
+        QString strError=db.lastError().text();
+        QMessageBox::information(this,"error",strError);
+        return;
+    }
+
+    QSqlQuery query;
+    if(query.exec("select datname from pg_database;"))
+    {
+        while(query.next())
+        {
+            QString strDb=query.value(0).toString();
+            qDebug()<<strDb<<endl;
+        }
+    }
+    db.close();
+}
+
+void MainWindow::selectAllDataBase(){
+    connected = db.open();
+    if(!connected)
+    {
+        QString strError=db.lastError().text();
+        QMessageBox::information(this,"error",strError);
+        return;
+    }
+
+    QSqlQuery query;
+    if(query.exec("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"))
+    {
+        while(query.next())
+        {
+            QString strDb=query.value(0).toString();
+            qDebug()<<strDb<<endl;
+        }
+    }
+    db.close();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -7,7 +7,9 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QSettings>
+
 #include "DBLogin.h"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -28,12 +30,23 @@ private:
     Ui::MainWindow *ui;
     DBLogin *dbLogin;
 
-    static QSqlDatabase *database;//数据库指针
-    static QSqlQuery *query;//数据库执行指针
+    QSettings *dbSettings;
+
+    QSqlDatabase db;//数据库
+    QSqlQuery *sqlQuery;//数据库执行指针
+    bool connected;
 
     QString configFileName;//配置文件名称
+    QString dataBasesName;
+
+    //查询所有数据库
+    void selectAllDataBase();
+
     //查询当前数据中的表
-    void selectTable();
+    void selectTable();    
+
+    //读取配置文件
+    void readINIFile();
 
 };
 #endif // MAINWINDOW_H
